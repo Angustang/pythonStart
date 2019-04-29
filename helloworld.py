4adf51e71f8cc4eaa5528d312b63387f22515a37
Angustang <angustang0305@foxmail.com>
2019-04-29 15:10:59 +0800

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#I am AngusTang
+import random
+
+zen = ["Beautiful is better than ugly","Explicit is better than implicit","Simple is better than complex",
+"Complex is better than complicated","Flat is better than nested","Sparse is better than dense","Now is better than never"]
+R = random.randint(0,6)
+print(zen[R])
